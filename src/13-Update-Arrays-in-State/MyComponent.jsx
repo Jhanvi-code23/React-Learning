8c5cfa24408c00d0e85aa1223fc0c14b0a62f6e2
Jhanvi <jhanvigupta892@gmail.com>
2026-08-17 20:46:58 +0530

    added

--- src/13-Update-Arrays-in-State/MyComponent.jsx
+++ src/13-Update-Arrays-in-State/MyComponent.jsx
@@ -1,28 +1,33 @@
 import React, {useState} from "react";
 function MyComponent() {
-    const[foods,setFoods] = useState(["Pizza","Burger","Pasta"]);
+    const[foods, setFoods] = useState(["Pizza","Burger","Pasta"]);
 
     //we will create 2 functions- 1. to add a new food item to the array and 2. to remove a food item from the array
     
     function handleAddFood() {
         const newFood= document.getElementById("foodInput").value;
         document.getElementById("foodInput").value = "";
-        
-        setFoods([newFood]); //spread operator is used to copy the existing array and add the new food item to it
+        setFoods([...foods, newFood]);
     }
-    const handleRemoveFood = () => {}
+
+
+    function handleRemoveFood(index) {
+        // Implementation for removing a food item
+                setFoods(foods.filter((element,i) => i !== index)); //removes food by clicking on the food item itself
+    }
 
     return(
         <div>
             <h1>Food Items</h1>
             <ul>
                 {foods.map((food,index) =>
-                    <li key={index}>{food}</li>
-                )}
+                    <li key={index} onClick={() => handleRemoveFood(index)}>
+                        {food}
+                    </li>
+                )} <br/>
 
-                <input type="text" placeholder="Enter a food item" />
+                <input id="foodInput" type="text" placeholder="Enter a food item" />
                 <button onClick={handleAddFood}>Add Food</button>
-                <button onClick={handleRemoveFood}>Remove Food</button>
             </ul>
         </div>
     )
